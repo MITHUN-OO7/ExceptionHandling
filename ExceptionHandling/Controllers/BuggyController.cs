538f1a8dc3fea1077626c3b34c1b0fe011727a5c
Mithun Kumar <mithunkiet@gmail.com>
2022-01-06 09:54:24 +0530

    Add project files.

---
+++ ExceptionHandling/Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+         
+        public BuggyController( )
+        {
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            // return "secret text";
+            return Unauthorized("Unauthorized");
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            string thingToReturn1= null;
+            var thingToReturn = thingToReturn1;
+            thingToReturn1.ToString();
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            string thingToReturn1 = null;
+            var thingToReturn = thingToReturn1;
+            thingToReturn1.ToString();
+            return thingToReturn;
+             
+        }
+
+
+    }
+}
---
+++ ExceptionHandling/Middleware/ExeptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using ExceptionHandling.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling.Middleware
+{
+    public class ExeptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExeptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExeptionMiddleware(RequestDelegate next, ILogger<ExeptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var response = _env.IsDevelopment() ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString()) : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, option);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
